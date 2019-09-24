408867b2ef186cf3f005d4f200f7032da4ba1815
Hideki Tsuchiya <htsuchiya@htsuchiya.local>
2019-09-25 06:53:16 +0900

    新規登録処理実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,8 +6,20 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
   end
 
   def edit
   end
+
+  private
+    def task_params
+      params.require(:task).permit(:name, :description)
+    end
 end

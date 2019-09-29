0cd29c1b3b943b3bd3ebba31d27ab249e94591c8
hideki-tsuchiya <h.tsuchi777@gmail.com>
2019-09-30 06:17:35 +0900

    add validates

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,10 @@
 class Task < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+
+  private
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end

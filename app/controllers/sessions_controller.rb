03a076bad335f41b75acf2dee4bec73064579718
hideki-tsuchiya <h.tsuchi777@gmail.com>
2019-10-14 09:42:52 +0900

    fix typo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,6 +21,6 @@ class SessionsController < ApplicationController
   private
 
   def session_params
-    params.require(:session).permit(:email, password)
+    params.require(:session).permit(:email, :password)
   end
 end

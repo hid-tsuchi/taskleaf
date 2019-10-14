5a379d894f73e5fb8a5d32a67b439d367d863695
hideki-tsuchiya <h.tsuchi777@gmail.com>
2019-10-14 09:23:30 +0900

    fix typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if settion[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 end

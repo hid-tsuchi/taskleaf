5e8d9e2ebd3293bce7077667a4033c55212c2d27
hideki-tsuchiya <h.tsuchi777@gmail.com>
2019-10-20 09:16:39 +0900

    ログインユーザーのタスクのみ表示できるよう修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,8 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
+  belongs_to :user
+
   private
 
   def validate_name_not_including_comma
